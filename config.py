2dc27a6cd8a65b5be3a15a4bca44003c33a15954
agrin10 <agrinflame2@gmail.com>
2025-06-07 17:12:11 +0330

    modified config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ class Config:
     TESTING =False
     SECRET_KEY = 'app_serete_key'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://oms_user:M|W(Y1D49Btd5X3S@localhost:3306/oms_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
